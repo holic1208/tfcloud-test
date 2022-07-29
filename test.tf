861c75eb592b20d65f812610d5f70e45938ad5ff
SSP-한상운 <SSP-SANGUN.HAN@hwk.net>
2022-07-29 13:59:01 +0900

    step.2

--- test.tf
+++ test.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = "ap-northeast-2"  
 }
 
+
+#######################################################
+## Keypair                                           ##
+#######################################################
+
+data "aws_key_pair" "key" { 
+  key_name = "aws_k8s_test"
+  include_public_key = true
+}
+
+
+#######################################################
+## vpc                                               ##
+#######################################################
+
 data "aws_vpc" "vpc-test" {
   id = "vpc-00bb4e7b1764e3235"
 }
@@ -34,4 +49,122 @@ resource "aws_subnet" "sub-test4" {
   tags = {
     Name = "test-sub-d"
   }
+}
+
+
+#######################################################
+## Security group & EC2                              ##
+#######################################################
+
+data "aws_security_group" "k8s-sg" {
+  id = "sg-0957b2f892bab58ad"
+}
+
+resource "aws_security_group_rule" "k8s-sg-Bastion1" {
+  description = "first connection"
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "TCP"
+  cidr_blocks = "58.151.93.20/32"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-Bastion2" {
+  description = "connection port"
+  type = "ingress"
+  from_port = 2022
+  to_port = 2022
+  protocol = "TCP"
+  cidr_blocks = "58.151.93.20/32"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-echo" {
+  description = "ICMP"
+  type = "ingress"
+  from_port = -1
+  to_port = -1
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-web" {
+  description = "web port"
+  type = "ingress"
+  from_port = 8080
+  to_port = 8080
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-k8s_Cni" {
+  description = "CNI-Weave API"
+  type = "ingress"
+  from_port = 6783
+  to_port = 6783
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-k8s_Api1" {
+  description = "k8s API"
+  type = "ingress"
+  from_port = 6443
+  to_port = 6443
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-k8s_Api2" {
+  description = "kubelet"
+  type = "ingress"
+  from_port = 10250
+  to_port = 10250
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-Api4" {
+  description = "controller manager"
+  type = "ingress"
+  from_port = 10257
+  to_port = 10257
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-Api3" {
+  description = "kube-schedular"
+  type = "ingress"
+  from_port = 10259
+  to_port = 10259
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group_rule" "k8s-sg-Nodeport" {
+  description = "NodePort Port"
+  type = "ingress"
+  from_port = 30000-32494
+  to_port = 30000-32494
+  protocol = "TCP"
+  cidr_blocks = "0.0.0.0/0"
+  security_group_id = data.aws_security_group.k8s-sg.id
+}
+
+resource "aws_security_group" "k8s-sg-outbound" {
+  description = "outbound rule"
+  type = "egress"
+  from_port = 0
+  to_prot = 0
+  protocol = "all"
+  security_group_id = data.aws_security_group.k8s-sg.id
 }
